187bcc23dcdad5bbb7b105e86f6f8b64ebc1e215
yoaghamdi <blackbox@example.com>
2026-07-09 13:28:32 +0300

    Force fix admin user password and permissions

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# 🌟 إنشاء حساب الأدمن تلقائياً إن لم يكن موجوداً
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); u, created = User.objects.get_or_create(username='admin', defaults={'email': 'admin@example.com'}); u.set_password('123456'); u.is_staff = True; u.is_superuser = True; u.save()"
+# 🌟 إعادة ضبط وتحديث حساب الأدمن وتأكيد الصلاحيات وكلمة المرور
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); u, _ = User.objects.get_or_create(username='admin'); u.set_password('123456'); u.is_staff = True; u.is_superuser = True; u.is_active = True; u.save(); print('ADMIN ACCOUNT UPDATED SUCCESSFULLY!')"
