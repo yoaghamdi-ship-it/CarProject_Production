1c4f517e1ebf7c7bb162551c6009f0b0fe37f50d
yoaghamdi-ship-it <yoaghamdi@gmail.com>
2026-08-20 13:06:59 +0300

    Force DEBUG True to reveal error details

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-av=1l$m2=bk^2s3nmm+)h7hks0z5e4ejs^4o3q29gbs4sy2kxt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
